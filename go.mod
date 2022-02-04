528a573115bd08689071ad5e4b5eb140a4fd574b
Gustavo Cioccari <gustavocioccari@gmail.com>
2022-02-04 11:41:29 -0300

    feat: add kafka consumer

--- go.mod
+++ go.mod
@@ -3,3 +3,10 @@ module mailservice
 go 1.17
 
 require github.com/joho/godotenv v1.4.0
+
+require (
+	github.com/golang/snappy v0.0.1 // indirect
+	github.com/klauspost/compress v1.9.8 // indirect
+	github.com/pierrec/lz4 v2.6.0+incompatible // indirect
+	github.com/segmentio/kafka-go v0.4.27 // indirect
+)
--- sendMail.go
+++ sendMail.go
@@ -1,46 +1,77 @@
 package main
 
 import (
+	"context"
+	"encoding/json"
+	"fmt"
 	"log"
 	"net/smtp"
 	"os"
 	"strings"
+	"time"
 
 	"github.com/joho/godotenv"
+	"github.com/segmentio/kafka-go"
 )
 
-func loadEnv() {
+type User struct {
+	ID        string    `json:"id"`
+	Email     string    `json:"email"`
+	CreatedAt time.Time `json:"created_at"`
+	UpdatedAt time.Time `json:"updated_at"`
+}
+
+func init() {
 	err := godotenv.Load()
 	if err != nil {
 		log.Fatal("Error loading .env file")
 	}
 }
 
-const ()
-
-var toAddress = []string{"guto.ps34@gmail.com", "gustavocioccari@gmail.com"}
-
-func SendEmail(message string, toAddress []string) (response bool, err error) {
-	loadEnv()
-
+func sendEmail(message string, toAddress string) (response bool, err error) {
 	fromAddress := os.Getenv("EMAIL")
 	fromEmailPassword := os.Getenv("PASSWORD")
 	smtpServer := os.Getenv("SMTP_SERVER")
 	smptPort := os.Getenv("SMTP_PORT")
 
 	var auth = smtp.PlainAuth("", fromAddress, fromEmailPassword, smtpServer)
-	err = smtp.SendMail(smtpServer+":"+smptPort, auth, fromAddress, toAddress, []byte(message))
-	log.Println(err)
+	err = smtp.SendMail(smtpServer+":"+smptPort, auth, fromAddress, []string{toAddress}, []byte(message))
 	if err == nil {
 		return true, nil
 	}
-	return false, err
 
+	return false, err
+}
+
+func consume(ctx context.Context) {
+	r := kafka.NewReader(kafka.ReaderConfig{
+		Brokers: []string{os.Getenv("KAFKA_BROKER")},
+		Topic:   "new-user",
+		GroupID: "email-new-users",
+	})
+
+	for {
+		msg, err := r.ReadMessage(ctx)
+		if err != nil {
+			panic("could not read message " + err.Error())
+		}
+		userData := msg.Value
+
+		var user User
+
+		err = json.Unmarshal(userData, &user)
+		if err != nil {
+			panic("could not parse userData " + err.Error())
+		}
+
+		subject := "Subject: Account created!\n\n"
+		body := fmt.Sprintf("You account is now active and your ID is %s. Congrats!", user.ID)
+		message := strings.Join([]string{subject, body}, " ")
+
+		sendEmail(message, user.Email)
+	}
 }
 
 func main() {
-	subject := "Subject: Subject\n\n"
-	body := "Body"
-	message := strings.Join([]string{subject, body}, " ")
-	SendEmail(message, toAddress)
+	consume(context.Background())
 }
